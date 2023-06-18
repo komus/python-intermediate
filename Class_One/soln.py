1ed8652185d5a7009bd73d1e66f4090af04087fe
komus <oyindolapokomolafe@yahoo.com>
2023-06-18 05:35:07 -0300

    solution

--- Class_One/soln.py
+++ Class_One/soln.py
@@ -1,8 +1,38 @@
 from collections import Counter
+from math import sqrt
 # Solution to Exercise
 
 # Question One
+class Calculator:
+    def __init__(self):
+        self.memory = 0
 
+    def add(self, number):
+        self.memory += number
+        return self.memory
+
+    def subtraction(self, number):
+        pass
+
+    def multiply(self, number):
+        pass
+
+    def division(self, number):
+        pass
+    
+    def squareroot(self):
+        self.memory = sqrt(self.memory)
+        return self.memory
+
+    def reset(self):
+        self.memory = 0
+
+calc = Calculator()
+print(calc.add(10))
+print(calc.add(5.63))
+print(calc.squareroot())
+calc.reset() # memory = 0
+print(calc.add(2))
 
 # Question 2
 s1 = "Hellok"
@@ -22,13 +52,9 @@ def string_creation(s1, s2):
 
 string_creation(s1, s2)
 
-#OR
-# Use a class called Counter
 
-print(Counter(s2))
 
 # Question 3
-#postional argument
 nums = [1, 2,3,4,5,4,2,-9999, 8.9, 7,9,10]
 
 def sum_num_list(nums:list):
@@ -42,3 +68,12 @@ def sum_num_list(nums:list):
 
 print(sum_num_list(nums))
         
+def sum_num_args(*args):
+    sum = 0
+    for x in args:
+        if x != -9999:
+            sum += x # sum = sum + x
+        else:
+            break
+    return sum
+print(sum_num_args(2,3,4,6,7, 9.08,-9999, 13, 34))
